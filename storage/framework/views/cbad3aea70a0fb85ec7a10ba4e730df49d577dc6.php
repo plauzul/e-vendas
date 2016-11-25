9cc2fb28447094bf53d8b96ca8898b79a15b5f2b
unknown <henriq10@outlook.com>
2016-11-25 21:22:13 -0200

    commit

---
+++ storage/framework/views/cbad3aea70a0fb85ec7a10ba4e730df49d577dc6.php
@@ -0,0 +1,50 @@
+<div id="contact-page" class="container"><!--contact-page-->
+    <div class="bg">	
+        <div class="row">  	
+            <div class="col-sm-8">
+                <div class="contact-form">
+                    <h2 class="title text-center">Entrar em Contato</h2>
+                    <div class="status alert alert-success" style="display: none"></div>
+                    <form id="main-contact-form" class="contact-form row" name="contact-form" method="post">
+                        <div class="form-group col-md-6">
+                            <input type="text" name="name" class="form-control" required="required" placeholder="Name">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <input type="email" name="email" class="form-control" required="required" placeholder="Email">
+                        </div>
+                        <div class="form-group col-md-12">
+                            <input type="text" name="subject" class="form-control" required="required" placeholder="Subject">
+                        </div>
+                        <div class="form-group col-md-12">
+                            <textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="Your Message Here"></textarea>
+                        </div>                        
+                        <div class="form-group col-md-12">
+                            <input type="submit" name="submit" class="btn btn-primary pull-right" value="Submit">
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="contact-info">
+                    <h2 class="title text-center">Informações de Contato</h2>
+                    <address>
+                        <p>E-Vendas Inc.</p>
+                        <p>Distrito Federal Brasilia - BRASIL</p>
+                        <p>Ceilandia DF</p>
+                        <p>Telefone: 61 988779988</p>
+                        <p>Email: evendasofficial@evendas.com</p>
+                    </address>
+                    <div class="social-networks">
+                        <h2 class="title text-center">Redes Sociais</h2>
+                        <ul>
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                            <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>    			
+        </div>  
+    </div>	
+</div><!--/contact-page-->
